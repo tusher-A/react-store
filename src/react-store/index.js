fe34daeaedfc008ac25ad8d0e655d7f5e998b63d
tusher-ahamed <tusher.ruet13@gmail.com>
2020-02-21 17:15:37 +0600

    added index file to import react-store

---
+++ src/react-store/index.js
@@ -0,0 +1,6 @@
+import Revive, { useStore } from "./GlobalStore";
+import combineReducer from "./combineReducer";
+import createStore from "./createStore";
+
+export { combineReducer, createStore, useStore };
+export default Revive;
